99a0c9a6b817acfc335c60be8e27924617ff7384
Adaline <jiayinleong100@gmail.com>
2022-05-27 12:23:31 -0700

    added a second div box to the html page

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+// document.getElementById('app').innerHTML = "text";
 
 import "./styles.css";
-import "./patch-fetch";
+import "./patch-fetch.js";
 import * as Monaco from "monaco-editor/esm/vs/editor/editor.api";
 import * as Parser from "web-tree-sitter";
 import { 
@@ -22,23 +23,26 @@ domain Mapping
                                c is Component, Mapping(c, p) }), s > 100.
 }`;
 
+// document.getElementById('app').innerHTML = "text";
+
+
 (async () => {
-  Theme.load(require("./theme.json"));//"monaco-tree-sitter/themes/tomorrow")); //to use the 'tomorrow' theme
-
-  await Parser.init();
-  const parser = new Parser();
-  const Lang = await Parser.Language.load('tree-sitter-formula.wasm');
-  parser.setLanguage(Lang);
-
-  const language = new Language(require("./grammar.json"));
-  await language.init("http://localhost:9000/tree-sitter-formula.wasm", Parser);
-
-
-  window.editor = Monaco.editor.create(document.querySelector("#app"), {
-    value: formulaCode,
-    language: "formula"
-  });
-
-  window.editor = editor;
-  window.monacoTreeSitter = new MonacoTreeSitter(Monaco, editor, language);
-})();
+    Theme.load(require("./theme.json"));//"monaco-tree-sitter/themes/tomorrow")); //to use the 'tomorrow' theme
+  
+    await Parser.init();
+    const parser = new Parser();
+    const Lang = await Parser.Language.load('tree-sitter-formula.wasm');
+    parser.setLanguage(Lang);
+  
+    const language = new Language(require("./grammar.json"));
+    await language.init("http://localhost:9000/tree-sitter-formula.wasm", Parser);
+  
+  
+    window.editor = Monaco.editor.create(document.querySelector("#app"), {
+      value: formulaCode,
+      language: "formula"
+    });
+  
+    window.editor = editor;
+    window.monacoTreeSitter = new MonacoTreeSitter(Monaco, editor, language);
+  })();
